8e5779f696daec875054124eef2e748c2d6f682e
leotuor <leotuor@hotmail.com>
2025-02-06 12:11:41 -0300

    DatePicker Funcional

---
+++ src/app/components/DatePicker/DatePickerComponent.tsx
@@ -0,0 +1,44 @@
+"use client"
+import React, { useState, useEffect } from 'react'
+import dynamic from 'next/dynamic';
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
+import './datePickerComponent.css';
+
+const DatePicker = dynamic(() => import("react-date-picker"), { ssr: false });
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+function DatePickerComponent() {
+  const [dataChegada, setDataChegada] = useState<Value>(null);
+  const [dataSaida, setDataSaida] = useState<Value>(null);
+
+  useEffect(() => {
+    setDataChegada(new Date());
+  }, []);
+
+  useEffect(() => {
+    setDataSaida(new Date());
+  }, []);
+
+  return (
+    <div>
+      <DatePicker
+        onChange={setDataChegada}
+        value={dataChegada}
+        className="border border-gray-300 p-1 rounded"
+      />
+      <DatePicker
+        onChange={setDataSaida}
+        value={dataSaida}
+        className="border border-gray-300 p-1 rounded"
+      />
+    </div>
+  )
+}
+
+
+
+export default DatePickerComponent;
